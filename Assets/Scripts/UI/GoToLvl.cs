fdb14c9ef31aff309b7808dac7a2919a89694ed6
gustavo13 <gurodrigues1300@gmail.com>
2019-05-23 17:33:36 -0300

    link menu and lvls

---
+++ Assets/Scripts/UI/GoToLvl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class GoToLvl : MonoBehaviour
+{
+   
+    private string nameScene;
+
+
+
+    public void LevelPresses(string currentLvl)
+    {
+        nameScene = currentLvl;
+       
+    }
+
+
+    public void GoPress()
+    {
+        SceneManager.LoadScene(nameScene);
+    }
+
+}
