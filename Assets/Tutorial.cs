63483699857e564a928133699c0bb17fcff2c1de
tripydgamestudio <tripydgamestudio@gmail.com>
2020-01-22 08:34:24 -0300

    add-tutorial2

---
+++ Assets/Tutorial.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tutorial : MonoBehaviour
+{
+    [SerializeField]
+    private Image imageTutorial;
+    [SerializeField]
+    private Image imageFinger;
+
+    private int tutorial = 0;
+
+    // Start is called before the first frame update
+    void Start()
+
+    {
+        
+        tutorial = PlayerPrefs.GetInt("Tutorial");
+        
+        if (tutorial == 1)
+        {
+            CloseButton();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CloseButton()
+    {
+        tutorial = 1;
+        PlayerPrefs.SetInt("Tutorial", tutorial);
+
+        imageTutorial.enabled = false;
+        imageFinger.enabled = false;
+        GetComponent<Canvas>().enabled = false;
+    }
+}
